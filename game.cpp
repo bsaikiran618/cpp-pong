c92b161b02bfd3a327b47b8826cc2c6cd33f0fce
saikiran <bsaikiran618@gmail.com>
2020-03-28 18:58:12 +0530

    completed?

--- game.cpp
+++ game.cpp
@@ -4,12 +4,14 @@
 #include "paddle.h"
 #include "ball.h"
 #include <iostream>
+#include <string>
 #include <sstream>
 using namespace std;
 
 Game::Game(std::string s)
 {
 	frames_rendered = 0;
+	isComplete = false;
 	if(SDL_Init(SDL_INIT_VIDEO) < 0)
 	{
 		throw InitFailed();		
@@ -23,6 +25,7 @@ Game::Game(std::string s)
 	TitleFont = TTF_OpenFont("./arial.ttf",26);
 	SubtitleFont = TTF_OpenFont("./arial.ttf",20);
 	ScoreFont = TTF_OpenFont("./arial.ttf",15);
+	ResultFont = TTF_OpenFont("./arial.ttf",26);
 
 	window = SDL_CreateWindow(s.c_str(),WIN_X,WIN_Y,WIN_W,WIN_H,SDL_WINDOW_SHOWN);
 	surface = SDL_GetWindowSurface(window);
@@ -43,37 +46,8 @@ void Game::renderNextFrame()
 	if(frames_rendered < MESSAGE_DISPLAY_FRAMES)
 	{
 
-		SDL_Color White = {255, 255, 255};
-		SDL_Surface* surfaceMessage = TTF_RenderText_Solid(TitleFont, "PONG++", White);
-
-		if(surfaceMessage == NULL){cout << "cannot create surface!" << endl;}
-		SDL_Texture* Message = SDL_CreateTextureFromSurface(renderer, surfaceMessage); 
-
-		if(Message == NULL){cout << "cannot create texture!" << endl;}
-
-		SDL_Rect Message_rect; //create a rect
-		Message_rect.x = (WIN_W/2) - 70;  //controls the rect's x coordinate 
-		Message_rect.y = (WIN_H/3) - 100; // controls the rect's y coordinte
-		Message_rect.w = 200; // controls the width of the rect
-		Message_rect.h = 70; // controls the height of the rect
-		SDL_RenderCopy(renderer, Message, NULL, &Message_rect); 
-
-
-		surfaceMessage = TTF_RenderText_Solid(SubtitleFont, "-created by boddulus", White);
-
-		if(surfaceMessage == NULL){cout << "cannot create surface!" << endl;}
-		Message = SDL_CreateTextureFromSurface(renderer, surfaceMessage); 
-
-		if(Message == NULL){cout << "cannot create texture!" << endl;}
-
-		Message_rect.x = (WIN_W/2) - 40;  //controls the rect's x coordinate 
-		Message_rect.y = (WIN_H/3); // controls the rect's y coordinte
-		Message_rect.w = 200; // controls the width of the rect
-		Message_rect.h = 50; // controls the height of the rect
-		SDL_RenderCopy(renderer, Message, NULL, &Message_rect);
-
-		SDL_FreeSurface(surfaceMessage);
-		SDL_DestroyTexture(Message);
+		displayMessage(TitleFont,SDL_Color{255,255,255},"Pong++",TITLE_X,TITLE_Y,TITLE_W,TITLE_H);
+		displayMessage(SubtitleFont,SDL_Color{0,0,255},"created by Boddulus",SUBT_X,SUBT_Y,SUBT_W,SUBT_H);
 	}	
 	
 	ball->move();
@@ -82,8 +56,20 @@ void Game::renderNextFrame()
 	right_paddle->render(renderer);
 	ball->render(renderer);
 	displayScore();
+
+	if(left_paddle->score == GAME_MAX_SCORE)
+	{
+		displayMessage(ResultFont,SDL_Color{255,255,0},"WINS!",LEFT_RES_X,LEFT_RES_Y,LEFT_RES_W,LEFT_RES_H);
+		isComplete = true;
+	}
+	else if(right_paddle->score == GAME_MAX_SCORE)
+	{
+		displayMessage(ResultFont,SDL_Color{255,255,0},"WINS!",RIGHT_RES_X,RIGHT_RES_Y,RIGHT_RES_W,RIGHT_RES_H);
+		isComplete = true;
+	}
+
 	SDL_RenderPresent(renderer);
-	if(frames_rendered < MESSAGE_DISPLAY_FRAMES)
+	if(frames_rendered <= MESSAGE_DISPLAY_FRAMES)
 		frames_rendered++;
 }
 void Game::startGame()
@@ -118,54 +104,48 @@ void Game::startGame()
 			}
 
 		}
-		renderNextFrame();
+		if(!isComplete)
+		{
+			renderNextFrame();
+		}
 	}
 }
 void Game::displayScore()
 {
-
-		SDL_Color White = {0, 255, 0};
-		ostringstream os1;
-		ostringstream os2;
-
-
-		os1 << "SCORE: " << left_paddle->score;
-		os2 << "SCORE: " << right_paddle->score;
+	SDL_Color White = {0, 255, 0};
+	ostringstream os1;
+	ostringstream os2;
 
 
+	os1 << "SCORE: " << left_paddle->score;
+	os2 << "SCORE: " << right_paddle->score;
 
-		SDL_Surface* surfaceMessage = TTF_RenderText_Solid(ScoreFont, os1.str().c_str() , White);
-		
-		if(surfaceMessage == NULL){cout << "cannot create font surface!" << endl;}
+	displayMessage(ScoreFont,SDL_Color{0,255,0},os1.str(),10,10,100,50);
+	displayMessage(ScoreFont,SDL_Color{0,255,0},os2.str(),WIN_W-100,10,100,50);
+}
+void Game::displayMessage(TTF_Font *font,SDL_Color color,string msg,int x,int y,int w, int h)
+{
+		SDL_Surface* surfaceMessage = TTF_RenderText_Solid(font, msg.c_str(), color);
+		if(surfaceMessage == NULL)
+		{
+			cerr << "couldnt create font surface!" << endl;
+			return;	
+		}
 		SDL_Texture* Message = SDL_CreateTextureFromSurface(renderer, surfaceMessage); 
 
-		if(Message == NULL){cout << "cannot create font texture!" << endl;}
+		if(Message == NULL)
+		{
+			cerr << "couldnt create font texture!" << endl;
+			return;
+		}
 
 		SDL_Rect Message_rect; //create a rect
-		Message_rect.x = 10;  //controls the rect's x coordinate 
-		Message_rect.y = 10; // controls the rect's y coordinte
-		Message_rect.w = 100; // controls the width of the rect
-		Message_rect.h = 30; // controls the height of the rect
-
-		SDL_RenderCopy(renderer, Message, NULL, &Message_rect); 
-
-		surfaceMessage = TTF_RenderText_Solid(ScoreFont, os2.str().c_str() , White);
-		
-		if(surfaceMessage == NULL){cout << "cannot create font surface!" << endl;}
-		Message = SDL_CreateTextureFromSurface(renderer, surfaceMessage); 
-
-		if(Message == NULL){cout << "cannot create font texture!" << endl;}
-
-		Message_rect.x = WIN_W - 120;  //controls the rect's x coordinate 
-		Message_rect.y = 10; // controls the rect's y coordinte
-		Message_rect.w = 100; // controls the width of the rect
-		Message_rect.h = 30; // controls the height of the rect
-
-		SDL_RenderCopy(renderer, Message, NULL, &Message_rect); 
-		
-
+		Message_rect.x = x; 
+		Message_rect.y = y;
+		Message_rect.w = w;
+		Message_rect.h = h;
+		SDL_RenderCopy(renderer, Message, NULL, &Message_rect);
 
 		SDL_FreeSurface(surfaceMessage);
 		SDL_DestroyTexture(Message);
-
 }
--- game.h
+++ game.h
@@ -5,8 +5,30 @@
 #define WIN_Y SDL_WINDOWPOS_UNDEFINED
 #define WIN_W 640
 #define WIN_H 480
+
+#define TITLE_X (WIN_W/2 - 70)
+#define TITLE_Y 100
+#define TITLE_W 200
+#define TITLE_H 100
+
+#define SUBT_X (WIN_W/2 - 200)
+#define SUBT_Y 200
+#define SUBT_W 400
+#define SUBT_H 50
+
+#define LEFT_RES_X 10
+#define LEFT_RES_Y WIN_H/2
+#define LEFT_RES_W 100
+#define LEFT_RES_H 70
+
+#define RIGHT_RES_X (WIN_W/2) + 400
+#define RIGHT_RES_Y WIN_H/2
+#define RIGHT_RES_W 100
+#define RIGHT_RES_H 70
+
 #define GAME_TITLE "My Window"
 #define MESSAGE_DISPLAY_FRAMES 150
+#define GAME_MAX_SCORE 5
 #include <string>
 #include "paddle.h"
 #include "ball.h"
@@ -15,12 +37,13 @@
 class Game
 {
 	private:
-		TTF_Font *TitleFont,*SubtitleFont,*ScoreFont;
+		TTF_Font *TitleFont,*SubtitleFont,*ScoreFont,*ResultFont;
 		SDL_Window *window;
 		SDL_Surface *surface;
 		SDL_Renderer *renderer;
 		Paddle *left_paddle,*right_paddle;
 		Ball *ball;
+		bool isComplete;
 	public:
 		int frames_rendered;
 		class InitFailed
@@ -28,10 +51,11 @@ class Game
 			public:
 				InitFailed()
 				{
-				
+
 				}
 		};
 		Game(std::string s);
+		void displayMessage(TTF_Font *,SDL_Color,std::string ,int ,int ,int , int );
 		void startGame();
 		void renderNextFrame();
 		void displayScore();
